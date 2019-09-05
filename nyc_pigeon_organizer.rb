91caf20c6036d08b7972b89682ed9d5e45dc1723
Solomon R Pena <solomonraulpena@gmail.com>
2019-09-05 21:11:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,5 +13,15 @@
 #  pigeon_list
 # end
 
-
- 
+def nyc_pigeon_organizer(data)
+  # write your code here!	  new_hash = {}
+end	  data.each do |characteristic, specifics|
+    specifics.each do |specifics, bird|
+      bird.each do |bird|
+        new_hash[bird] ||= {}
+        new_hash[bird][characteristic] ||= []
+        new_hash[bird][characteristic] << specifics.to_s
+      end
+    end
+  end
+    new_hash
