b01cfa216fa5a48dfd9fd38edfe624117d4f1a9f
Solomon R Pena <solomonraulpena@gmail.com>
2019-09-05 21:05:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,8 @@ def nyc_pigeon_organizer(data)
 pigeon_list = {}
 data.each do |attributes,details|
   attributes.each do |attributes,name|
-    name.each do |name|={}
+    name.each do |name|
+      
       pigeon_list [name][attributes] ||={}
 
 
