efa312a0f2fdccb80d708495975bdd825accb520
Solomon R Pena <solomonraulpena@gmail.com>
2019-09-05 20:20:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,44 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-data = new_data
+# data = new_data
+# pigeon_list = {
+# names:{Theo:{},pete:{},Lucky:{},Ms_K:{},Queenie:{},Andrew:{},Alex:{}
+# }
 pigeon_list = {
-names:{Theo:{},pete:{},Lucky:{},Ms_K:{},Queenie:{},Andrew:{},Alex:{}
+  "Theo" => {
+    :color => ["purple", "grey"],
+    :gender => ["male"],
+    :lives => ["Subway"]
+  },
+  "Peter Jr." => {
+    :color => ["purple", "grey"],
+    :gender => ["male"],
+    :lives => ["Library"]
+  },
+  "Lucky" => {
+    :color => ["purple"],
+    :gender => ["male"],
+    :lives => ["Central Park"]
+  },
+  "Ms. K" => {
+    :color => ["grey", "white"],
+    :gender => ["female"],
+    :lives => ["Central Park"]
+  },
+  "Queenie" => {
+    :color => ["white", "brown"],
+    :gender => ["female"],
+    :lives => ["Subway"]
+  },
+  "Andrew" => {
+    :color => ["white"],
+    :gender => ["male"],
+    :lives => ["City Hall"]
+  },
+  "Alex" => {
+    :color => ["white", "brown"],
+    :gender => ["male"],
+    :lives => ["Central Park"]
+  }
 }
-
 end
