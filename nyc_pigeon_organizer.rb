6a4babbbf9bb8cb10956f277f827152b1eb23d81
Solomon R Pena <solomonraulpena@gmail.com>
2019-09-05 20:33:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,46 +1,10 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-# data = new_data
-# pigeon_list = {
-# names:{Theo:{},pete:{},Lucky:{},Ms_K:{},Queenie:{},Andrew:{},Alex:{}
-# }
-
-
+data = new_data
 pigeon_list = {
-  "Theo" => {
-    :color => ["purple", "grey"],
-    :gender => ["male"],
-    :lives => ["Subway"]
-  },
-  "Peter Jr." => {
-    :color => ["purple", "grey"],
-    :gender => ["male"],
-    :lives => ["Library"]
-  },
-  "Lucky" => {
-    :color => ["purple"],
-    :gender => ["male"],
-    :lives => ["Central Park"]
-  },
-  "Ms. K" => {
-    :color => ["grey", "white"],
-    :gender => ["female"],
-    :lives => ["Central Park"]
-  },
-  "Queenie" => {
-    :color => ["white", "brown"],
-    :gender => ["female"],
-    :lives => ["Subway"]
-  },
-  "Andrew" => {
-    :color => ["white"],
-    :gender => ["male"],
-    :lives => ["City Hall"]
-  },
-  "Alex" => {
-    :color => ["white", "brown"],
-    :gender => ["male"],
-    :lives => ["Central Park"]
-  }
+names:{Theo:{},pete:{},Lucky:{},Ms_K:{},Queenie:{},Andrew:{},Alex:{}
 }
+
+
+
 end
