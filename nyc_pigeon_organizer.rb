a0784fdf64a79c3f1c016f5110b8a4ee18957a57
Solomon R Pena <solomonraulpena@gmail.com>
2019-09-05 21:10:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,30 @@
-def nyc_pigeon_organizer(data)
-pigeon_list = {}
-data.each do |attributes,details|
-  attributes.each do |attributes,name|
-    name.each do |name|
-     pigeon_list[name] ||={}
-     pigeon_list[name][attributes]||=[]
-     pigeon_list[name][attributes]<< details.to_s
+# def nyc_pigeon_organizer(data)
+# pigeon_list = {}
+# data.each do |attributes,details|
+#   attributes.each do |attributes,name|
+#     name.each do |name|
+#      pigeon_list[name] ||={}
+#      pigeon_list[name][attributes]||=[]
+#      pigeon_list[name][attributes]<< details.to_s
+#
+#    end
+#   end
+#  end
+#  pigeon_list
+# end
 
-   end
+
+
+def nyc_pigeon_organizer(data)
+  # write your code here!	  new_hash = {}
+end	  data.each do |characteristic, specifics|
+    specifics.each do |specifics, bird|
+      bird.each do |bird|
+        new_hash[bird] ||= {}
+        new_hash[bird][characteristic] ||= []
+        new_hash[bird][characteristic] << specifics.to_s
+      end
+    end
   end
- end
- pigeon_list
-end
+    new_hash
+end 
